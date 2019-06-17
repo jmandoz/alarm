60af8ecf5907341eeedbbf682a7545c0e0903287
Jason Mandozzi <jason.mandozzi@gmail.com>
2019-06-17 17:08:53 -0600

    Added the cutoms table View cell

---
+++ Alarm/View/Custom Views/SwitchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SwitchTableViewCell.swift
+//  Alarm
+//
+//  Created by Jason Mandozzi on 6/17/19.
+//  Copyright © 2019 Jason Mandozzi. All rights reserved.
+//
+
+import UIKit
+
+class SwitchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
