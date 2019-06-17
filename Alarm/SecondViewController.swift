a37beecfa44301a27187dd1e2260f35d015f2ca2
Jason Mandozzi <jason.mandozzi@gmail.com>
2019-06-17 13:49:59 -0600

    Initial Commit

---
+++ Alarm/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Alarm
+//
+//  Created by Jason Mandozzi on 6/17/19.
+//  Copyright © 2019 Jason Mandozzi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
